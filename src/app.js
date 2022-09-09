b3a7f0f773c59283105744d9841c732daeb3ebf9
francomammoli <francomammoli@hotmail.com>
2022-09-09 01:17:24 -0300

    update

--- src/app.js
+++ src/app.js
@@ -1,11 +1,10 @@
 const express = require('express');
 const app = express();
-
 //settings 
 app.set('port',process.env.PORT || 3000);
 
 //volvemos accesible la capeta public para todo el proyecto
-app.use(express.static('public'));
+app.use(express.static('./public'));
 //le aclaramos a express cual es el motor de plantillas que vamos a usar
 app.set('view engine','ejs');
 app.set('views','./public/views');
